84faa622c5d1bb215717da0c0f3d78028aae8479
hiren-chauhan <hirenjchauhan@hotmail.com>
2025-04-30 12:27:34 +0530

    init

---
+++ day1/variable.php
@@ -0,0 +1,20 @@
+<?php
+// create variable
+$var = 'A';
+echo "Here is Variable___: " . $var ;
+ 
+
+$name = "Mahi";
+$age = 22;
+$price = 9.99;
+$isAvailable = true;
+
+echo "<br>Name: $name, Age: $age";
+
+// String Concatenation
+$name = "Mahi";
+echo "<br>Hello " . $name;  // Dot (.) is used to join strings
+
+
+
+?>
